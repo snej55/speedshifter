0583d88feb2859d0d45c4e58f91284e74bca0bed
Jens Kromdijk <jenskromdijk@gmail.com>
2025-04-11 17:40:52 +0100

    Add Qt application

--- src/main.cpp
+++ src/main.cpp
@@ -7,3 +7,29 @@
 #include <KLocalizedString>
 #include <KIconTheme>
 
+int main(int argc, char* argv[])
+{
+    KIconTheme::initTheme();
+    QApplication app(argc, argv);
+    KLocalizedString::setApplicationDomain("speedshifter");
+    QApplication::setApplicationName(QStringLiteral("Speed Shifter"));
+    QApplication::setDesktopFileName(QStringLiteral("org.kde.speedshifter.desktop"));
+
+    QApplication::setStyle(QStringLiteral("breeze"));
+    if (qEnvironmentVariableIsEmpty("QT_QUICK_CONTROLS_STYLE"))
+    {
+        QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+    }
+
+    QQmlApplicationEngine engine;
+
+    engine.rootContext()->setContextObject(new KLocalizedContext(&engine));
+    engine.loadFromModule("org.kde.speedshifter", "Main");
+
+    if (engine.rootObjects().isEmpty())
+    {
+        return -1;
+    }
+
+    return app.exec();
+}
