04f35395b9195d8bb8a6719b4710855d0d04cb8a
Jens Kromdijk <jenskromdijk@gmail.com>
2025-04-11 17:21:49 +0100

    Update .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <QApplication>
+#include <QQmlApplicationEngine>
+#include <QtQml>
+#include <QUrl>
+#include <QQuickStyle>
+#include <KLocalizedContext>
+#include <KLocalizedString>
+#include <KIconTheme>
+
