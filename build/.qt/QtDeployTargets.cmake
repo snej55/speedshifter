86cf483056389b66ec3f88e5b2afd16d4a35a2a5
Jens Kromdijk <jenskromdijk@gmail.com>
2026-05-16 17:41:31 +0100

    Remove build folder

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_main_FILE /home/jenskromdjk/projects/speedshifter/build/src/main)
-set(__QT_DEPLOY_TARGET_main_TYPE EXECUTABLE)
--- build/src/.qt/main_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "main")
-set(working_dir "/home/jenskromdjk/projects/speedshifter/src")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/jenskromdjk/projects/speedshifter/build/src/.qt/main_res.txt")
